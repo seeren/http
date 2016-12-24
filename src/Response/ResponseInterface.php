162f586b0e733f5e7d812bb6966d8ce58422e411
Cyril <consultant@seeren.fr>
2016-12-24 18:00:41 +0100

    Coding style

--- src/Response/ResponseInterface.php
+++ src/Response/ResponseInterface.php
@@ -31,7 +31,7 @@ interface ResponseInterface extends MessageInterface
        /**
         * @var string header value
         */
-       HEADER_ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin",
+       HEADER_ACCESS_CONTROL_ALLOW_ORIGIN      = "Access-Control-Allow-Origin",
        /**
         * @var string header value
         */
@@ -39,250 +39,250 @@ interface ResponseInterface extends MessageInterface
        /**
         * @var string header value
         */
-       HEADER_ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers",
+       HEADER_ACCESS_CONTROL_ALLOW_HEADERS     = "Access-Control-Allow-Headers",
        /**
         * @var string header value
         */
-       HEADER_ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Headers",
+       HEADER_ACCESS_CONTROL_ALLOW_METHODS     = "Access-Control-Allow-Headers",
        /**
         * @var string header value
         */
-       HEADER_ACCEPT_RANGES = "Accept-Ranges",
+       HEADER_ACCEPT_RANGES                    = "Accept-Ranges",
        /**
         * @var string header value
         */
-       HEADER_AGE = "Age",
+       HEADER_AGE                              = "Age",
        /**
         * @var string header value
         */
-       HEADER_ALLOW = "Allow",
+       HEADER_ALLOW                            = "Allow",
        /**
         * @var string header value
         */
-       HEADER_CONNECTION = "Connection",
+       HEADER_CONNECTION                       = "Connection",
        /**
         * @var string header value
         */
-       HEADER_CONTENT_DISPOSITION = "Content-Disposition",
+       HEADER_CONTENT_DISPOSITION              = "Content-Disposition",
        /**
         * @var string header value
         */
-       HEADER_CONTENT_ENCODING = "Content-Encoding",
+       HEADER_CONTENT_ENCODING                 = "Content-Encoding",
        /**
         * @var string header value
         */
-       HEADER_CONTENT_LANGUAGE = "Content-Language",
+       HEADER_CONTENT_LANGUAGE                 = "Content-Language",
        /**
         * @var string header value
         */
-       HEADER_ETAG = "ETag",
+       HEADER_ETAG                             = "ETag",
        /**
         * @var string header value
         */
-       HEADER_EXPIRES= "Expires",
+       HEADER_EXPIRES                          = "Expires",
        /**
         * @var string header value
         */
-       HEADER_LAST_MODIFIED = "Last-Modified",
+       HEADER_LAST_MODIFIED                    = "Last-Modified",
        /**
         * @var string header value
         */
-       HEADER_LOCATION = "Location",
+       HEADER_LOCATION                         = "Location",
        /**
         * @var string header value
         */
-       HEADER_PRAGMA = "Pragma",
+       HEADER_PRAGMA                           = "Pragma",
        /**
         * @var string header value
         */
-       HEADER_REFRESH = "Refresh",
+       HEADER_REFRESH                          = "Refresh",
        /**
         * @var string header value
         */
-       HEADER_RETRY_AFTER = "Retry-After",
+       HEADER_RETRY_AFTER                      = "Retry-After",
        /**
         * @var string header value
         */
-       HEADER_TRANSFERT_ENCODING = "Transfer-Encoding",
+       HEADER_TRANSFERT_ENCODING               = "Transfer-Encoding",
        /**
         * @var string header value
         */
-       HEADER_WWW_AUTHENTICATE = "WWW-Authenticate",
+       HEADER_WWW_AUTHENTICATE                 = "WWW-Authenticate",
        /**
         * @var string reason phrase
         */
-       STATUS_100 = "Continue",
+       STATUS_100                              = "Continue",
        /**
         * @var string reason phrase
         */
-       STATUS_101 = "Switching Protocols",
+       STATUS_101                              = "Switching Protocols",
        /**
         * @var string reason phrase
         */
-       STATUS_200 = "OK",
+       STATUS_200                              = "OK",
        /**
         * @var string reason phrase
         */
-       STATUS_201 = "Created",
+       STATUS_201                              = "Created",
        /**
         * @var string reason phrase
         */
-       STATUS_202 = "Accepted",
+       STATUS_202                              = "Accepted",
        /**
         * @var string reason phrase
         */
-       STATUS_203 = "Non-Authoritative Information",
+       STATUS_203                              = "Non-Authoritative Information",
        /**
         * @var string reason phrase
         */
-       STATUS_204 = "No Content",
+       STATUS_204                              = "No Content",
        /**
         * @var string reason phrase
         */
-       STATUS_205 = "Reset Content",
+       STATUS_205                              = "Reset Content",
        /**
         * @var string reason phrase
         */
-       STATUS_206 = "Partial Content",
+       STATUS_206                              = "Partial Content",
        /**
         * @var string reason phrase
         */
-       STATUS_300 = "Multiple Choices",
+       STATUS_300                              = "Multiple Choices",
        /**
         * @var string reason phrase
         */
-       STATUS_301 = "Moved Permanently",
+       STATUS_301                              = "Moved Permanently",
        /**
         * @var string reason phrase
         */
-       STATUS_302 = "Moved Temporarily",
+       STATUS_302                              = "Moved Temporarily",
        /**
         * @var string reason phrase
         */
-       STATUS_303 = "See Other",
+       STATUS_303                              = "See Other",
        /**
         * @var string reason phrase
         */
-       STATUS_304 = "Not Modified",
+       STATUS_304                              = "Not Modified",
        /**
         * @var string reason phrase
         */
-       STATUS_400  = "Bad Request",
+       STATUS_400                              = "Bad Request",
        /**
         * @var string reason phrase
         */
-       STATUS_401 = "Unauthorized",
+       STATUS_401                              = "Unauthorized",
        /**
         * @var string reason phrase
         */
-       STATUS_402 = " Payment Required",
+       STATUS_402                              = " Payment Required",
        /**
         * @var string reason phrase
         */
-       STATUS_403 = "Forbidden",
+       STATUS_403                              = "Forbidden",
        /**
         * @var string reason phrase
         */
-       STATUS_404 = "Not Found",
+       STATUS_404                              = "Not Found",
        /**
         * @var string reason phrase
         */
-       STATUS_405 = "Method Not Allowed",
+       STATUS_405                              = "Method Not Allowed",
        /**
         * @var string reason phrase
         */
-       STATUS_406 = "Not Acceptable",
+       STATUS_406                              = "Not Acceptable",
        /**
         * @var string reason phrase
         */
-       STATUS_407 = "Proxy Authentication Required",
+       STATUS_407                              = "Proxy Authentication Required",
        /**
         * @var string reason phrase
         */
-       STATUS_408 = "Request Timeout",
+       STATUS_408                              = "Request Timeout",
        /**
         * @var string reason phrase
         */
-       STATUS_409 = "Conflict",
+       STATUS_409                              = "Conflict",
        /**
         * @var string reason phrase
         */
-       STATUS_410 = "Gone",
+       STATUS_410                              = "Gone",
        /**
         * @var string reason phrase
         */
-       STATUS_411 = "Length Required",
+       STATUS_411                              = "Length Required",
        /**
         * @var string reason phrase
         */
-       STATUS_412 = "Precondition Failed",
+       STATUS_412                              = "Precondition Failed",
        /**
         * @var string reason phrase
         */
-       STATUS_413 = "Payload Too Large",
+       STATUS_413                              = "Payload Too Large",
        /**
         * @var string reason phrase
         */
-       STATUS_414 = "Request-URI Too Long",
+       STATUS_414                              = "Request-URI Too Long",
        /**
         * @var string reason phrase
         */
-       STATUS_415 = "Unsupported Media Type",
+       STATUS_415                              = "Unsupported Media Type",
        /**
         * @var string reason phrase
         */
-       STATUS_416 = "Requested Range Not Satisfiable",
+       STATUS_416                              = "Requested Range Not Satisfiable",
        /**
         * @var string reason phrase
         */
-       STATUS_417 = "Expectation Failed",
+       STATUS_417                              = "Expectation Failed",
        /**
         * @var string reason phrase
         */
-       STATUS_500 = "Internal Server Error",
+       STATUS_500                              = "Internal Server Error",
        /**
         * @var string reason phrase
         */
-       STATUS_501 = "Not Implemented",
+       STATUS_501                              = "Not Implemented",
        /**
         * @var string reason phrase
         */
-       STATUS_502 = "Bad Gateway",
+       STATUS_502                              = "Bad Gateway",
        /**
         * @var string reason phrase
         */
-       STATUS_503 = "Service Unavailable",
+       STATUS_503                              = "Service Unavailable",
        /**
         * @var string reason phrase
         */
-       STATUS_504 = "Gateway Timeout",
+       STATUS_504                              = "Gateway Timeout",
        /**
         * @var string reason phrase
         */
-       STATUS_505 = "HTTP Version Not Supported",
+       STATUS_505                              = "HTTP Version Not Supported",
        /**
         * @var string reason phrase
         */
-       STATUS_506 = "Variant Also Negotiates",
+       STATUS_506                              = "Variant Also Negotiates",
        /**
         * @var string reason phrase
         */
-       STATUS_507 = "Insufficient Storage",
+       STATUS_507                              = "Insufficient Storage",
        /**
         * @var string reason phrase
         */
-       STATUS_508 = "Loop Detected",
+       STATUS_508                              = "Loop Detected",
        /**
         * @var string reason phrase
         */
-       STATUS_509 = "Bandwidth Limit Exceeded",
+       STATUS_509                              = "Bandwidth Limit Exceeded",
        /**
         * @var string reason phrase
         */
-       STATUS_510 = "Not Extended",
+       STATUS_510                              = "Not Extended",
        /**
         * @var string reason phrase
         */
-       STATUS_520 = "Web server is returning an unknown error";
+       STATUS_520                              = "Web server is returning an unknown error";
 
 }

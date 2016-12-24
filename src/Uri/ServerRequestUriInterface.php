e39fe22eedb57b85a5ae7982b0b3dbfd533dc008
Cyril <consultant@seeren.fr>
2016-12-24 16:18:33 +0100

    Typo indentation

--- src/Uri/ServerRequestUriInterface.php
+++ src/Uri/ServerRequestUriInterface.php
@@ -10,7 +10,7 @@
  *
  * @copyright (c) Cyril Ichti <consultant@seeren.fr>
  * @link http://www.seeren.fr/ Seeren
- * @version 1.0.2
+ * @version 1.0.3
  */
 
 namespace Seeren\Http\Uri;
@@ -29,11 +29,11 @@ interface ServerRequestUriInterface extends UriInterface
        /**
         * @var string server index
         */
-       SERVER_SCHEME = "REQUEST_SCHEME",
+       SERVER_SCHEME        = "REQUEST_SCHEME",
        /**
         * @var string server index
         */
-       SERVER_USER = "REMOTE_USER",
+       SERVER_USER          = "REMOTE_USER",
        /**
         * @var string server index
         */
@@ -41,26 +41,26 @@ interface ServerRequestUriInterface extends UriInterface
        /**
         * @var string server index
         */
-       SERVER_HOST = "SERVER_NAME",
+       SERVER_HOST          = "SERVER_NAME",
        /**
         * @var string server index
         */
-       SERVER_PORT = "SERVER_PORT",
+       SERVER_PORT          = "SERVER_PORT",
        /**
         * @var string server index
         */
-       SERVER_PATH = "SCRIPT_NAME",
+       SERVER_PATH          = "SCRIPT_NAME",
        /**
         * @var string server index
         */
-       SERVER_QUERY = "QUERY_STRING",
+       SERVER_QUERY         = "QUERY_STRING",
        /**
         * @var string server index
         */
-       SERVER_REDIRECT_URL = "REDIRECT_URL",
+       SERVER_REDIRECT_URL  = "REDIRECT_URL",
        /**
         * @var string server index
         */
-       SERVER_REQUEST_URI = "REQUEST_URI";
+       SERVER_REQUEST_URI   = "REQUEST_URI";
 
 }

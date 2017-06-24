e78c2c7aa7720ddc69f393d24fde4d24049fc701
Cyril <consultant@seeren.fr>
2017-06-24 16:30:55 +0200

    Add coverage for upload

--- test/Upload/UploadedFileTest.php
+++ test/Upload/UploadedFileTest.php
@@ -9,8 +9,8 @@
  *    /_//_//_//_//_//_/
  *
  * @copyright (c) Cyril Ichti <consultant@seeren.fr>
- * @link http://www.seeren.fr/ Seeren
- * @version 1.0.2
+ * @link https://github.com/seeren/http
+ * @version 2.0.1
  */
 
 namespace Seeren\Http\Test\Upload;
@@ -27,7 +27,7 @@ use ReflectionClass;
  * @subpackage Upload\Test
  * @final
  */
-final class UploadedFileTest extends UploadedFileInterfaceTest
+final class UploadedFileTest extends AbstractUploadedFileTest
 {
 
    /**
@@ -37,15 +37,174 @@ final class UploadedFileTest extends UploadedFileInterfaceTest
     */
    protected final function getUploadedFile(): UploadedFileInterface
    {
-       $testFile = __DIR__ . DIRECTORY_SEPARATOR . "testFile.txt";
-       file_put_contents($testFile, "upload test");
+       $testFile = __DIR__ . DIRECTORY_SEPARATOR . "dummy.txt";
+       file_put_contents($testFile, "hello world");
        return (new ReflectionClass(UploadedFile::class))
               ->newInstanceArgs([[
-                  "name" => "testFile",
+                  "name" => "dummy",
                   "size" => null,
-                  "type" => null,
                   "tmp_name" => $testFile,
                   "error" => 0 ]]);
+   }
+
+   /**
+    * @covers \Seeren\Http\Upload\UploadedFile::__construct
+    * @covers \Seeren\Http\Stream\Stream::__construct
+    * @covers \Seeren\Http\Upload\UploadedFile::getError
+    */
+   public function testErrorNoFile()
+   {
+      $this->assertTrue(
+          (new ReflectionClass(UploadedFile::class))
+           ->newInstanceArgs([["tmp_name" => "bad tmp_name"]])
+           ->getError() === 4
+      );
+   }
+
+   /**
+    * @covers \Seeren\Http\Upload\UploadedFile::__construct
+    * @covers \Seeren\Http\Upload\UploadedFile::getStream
+    * @expectedException \RuntimeException
+    */
+   public function testGetStreamRuntimeException()
+   {
+       (new ReflectionClass(UploadedFile::class))
+       ->newInstanceArgs([[]])
+       ->getStream();
+   }
+
+   /**
+    * @covers \Seeren\Http\Upload\UploadedFile::__construct
+    * @covers \Seeren\Http\Upload\UploadedFile::getStream
+    * @covers \Seeren\Http\Upload\UploadedFile::moveTo
+    * @expectedException \RuntimeException
+    */
+   public function testMoveToRuntimeException()
+   {
+       (new ReflectionClass(UploadedFile::class))
+       ->newInstanceArgs([[]])
+       ->moveTo(__DIR__ . DIRECTORY_SEPARATOR . "moved.txt");
+   }
+
+   /**
+    * @covers \Seeren\Http\Upload\UploadedFile::__construct
+    * @covers \Seeren\Http\Stream\Stream::__construct
+    * @covers \Seeren\Http\Stream\Stream::getMetadata
+    * @covers \Seeren\Http\Stream\Stream::getSize
+    * @covers \Seeren\Http\Stream\Stream::setReadableWritable
+    * @covers \Seeren\Http\Upload\UploadedFile::getSize
+    */
+   public function testGetSize()
+   {
+       $this->assertTrue($this->getUploadedFile()->getSize() === 11);
+   }
+
+   /**
+    * @covers \Seeren\Http\Upload\UploadedFile::__construct
+    * @covers \Seeren\Http\Stream\Stream::__construct
+    * @covers \Seeren\Http\Stream\Stream::getMetadata
+    * @covers \Seeren\Http\Stream\Stream::getSize
+    * @covers \Seeren\Http\Stream\Stream::setReadableWritable
+    * @covers \Seeren\Http\Upload\UploadedFile::getClientFilename
+    */
+   public function testGetClientFilename()
+   {
+       $this->assertTrue(
+           $this->getUploadedFile()
+           ->getClientFileName() === "dummy");
+   }
+
+   /**
+    * @covers \Seeren\Http\Upload\UploadedFile::__construct
+    * @covers \Seeren\Http\Stream\Stream::__construct
+    * @covers \Seeren\Http\Stream\Stream::getMetadata
+    * @covers \Seeren\Http\Stream\Stream::getSize
+    * @covers \Seeren\Http\Stream\Stream::setReadableWritable
+    * @covers \Seeren\Http\Upload\UploadedFile::getClientMediaType
+    */
+   public function testGetClientMediaType()
+   {
+       $this->assertTrue(
+           $this->getUploadedFile()
+           ->getClientMediaType() === null);
+   }
+
+   /**
+    * @covers \Seeren\Http\Upload\UploadedFile::__construct
+    * @covers \Seeren\Http\Stream\Stream::__construct
+    * @covers \Seeren\Http\Stream\Stream::getMetadata
+    * @covers \Seeren\Http\Stream\Stream::getSize
+    * @covers \Seeren\Http\Stream\Stream::setReadableWritable
+    * @covers \Seeren\Http\Upload\UploadedFile::getStream
+    */
+   public function testGetStream()
+   {
+       parent::testGetStream();
+   }
+
+   /**
+    * @covers \Seeren\Http\Upload\UploadedFile::__construct
+    * @covers \Seeren\Http\Stream\Stream::__construct
+    * @covers \Seeren\Http\Stream\Stream::__toString
+    * @covers \Seeren\Http\Stream\Stream::close
+    * @covers \Seeren\Http\Stream\Stream::detach
+    * @covers \Seeren\Http\Stream\Stream::getContents
+    * @covers \Seeren\Http\Stream\Stream::getMetadata
+    * @covers \Seeren\Http\Stream\Stream::getSize
+    * @covers \Seeren\Http\Stream\Stream::isReadable
+    * @covers \Seeren\Http\Stream\Stream::setReadableWritable
+    * @covers \Seeren\Http\Upload\UploadedFile::getError
+    * @covers \Seeren\Http\Upload\UploadedFile::moveTo
+    * @covers \Seeren\Http\Upload\UploadedFile::getStream
+    */
+   public function testMoveTo()
+   {
+       parent::testMoveTo();
+   }
+
+   /**
+    * @covers \Seeren\Http\Upload\UploadedFile::__construct
+    * @covers \Seeren\Http\Stream\Stream::__construct
+    * @covers \Seeren\Http\Stream\Stream::getMetadata
+    * @covers \Seeren\Http\Stream\Stream::getSize
+    * @covers \Seeren\Http\Stream\Stream::setReadableWritable
+    * @covers \Seeren\Http\Upload\UploadedFile::moveTo
+    * @expectedException \InvalidArgumentException
+    */
+   public function testMoveToInvalidArgumentException()
+   {
+       parent::testMoveToInvalidArgumentException();
+   }
+
+   /**
+    * @covers \Seeren\Http\Upload\UploadedFile::__construct
+    * @covers \Seeren\Http\Stream\Stream::__construct
+    * @covers \Seeren\Http\Stream\Stream::__toString
+    * @covers \Seeren\Http\Stream\Stream::getContents
+    * @covers \Seeren\Http\Stream\Stream::getMetadata
+    * @covers \Seeren\Http\Stream\Stream::getSize
+    * @covers \Seeren\Http\Stream\Stream::isReadable
+    * @covers \Seeren\Http\Stream\Stream::setReadableWritable
+    * @covers \Seeren\Http\Upload\UploadedFile::getStream
+    * @covers \Seeren\Http\Upload\UploadedFile::moveTo
+    * @expectedException \RuntimeException
+    */
+   public function testMoveToRuntimeExceptionReadOnly()
+   {
+       parent::testMoveToRuntimeExceptionReadOnly();
+   }
+
+   /**
+    * @covers \Seeren\Http\Upload\UploadedFile::__construct
+    * @covers \Seeren\Http\Stream\Stream::__construct
+    * @covers \Seeren\Http\Stream\Stream::getMetadata
+    * @covers \Seeren\Http\Stream\Stream::getSize
+    * @covers \Seeren\Http\Stream\Stream::setReadableWritable
+    * @covers \Seeren\Http\Upload\UploadedFile::getError
+    */
+   public function testGetError()
+   {
+       parent::testGetError();
    }
 
 }

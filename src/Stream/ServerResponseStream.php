2448564445f9693031412724ec4af6d899e3e4bb
Cyril <consultant@seeren.fr>
2017-06-26 23:13:12 +0200

    Remove catch because can't be throw

--- src/Stream/ServerResponseStream.php
+++ src/Stream/ServerResponseStream.php
@@ -10,12 +10,10 @@
  *
  * @copyright (c) Cyril Ichti <consultant@seeren.fr>
  * @link https://github.com/seeren/http
- * @version 1.0.2
+ * @version 1.0.3
  */
 
 namespace Seeren\Http\Stream;
-
-use RuntimeException;
 
 /**
  * Class for represent server response stream
@@ -31,17 +29,10 @@ class ServerResponseStream extends Stream
     * Construct ServerResponseStream
     * 
     * @return null
-    * 
-    * @throws RuntimeException on faillure
     */
    public function __construct()
    {
-       try {
-           parent::__construct("php://output", self::MODE_W);
-       } catch (RuntimeException $e) {
-           throw new RuntimeException(
-               "Can't create ResponseStream: " . $e->getMessage());
-       }
+       parent::__construct("php://output", self::MODE_W);
    }
 
 }
